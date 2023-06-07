d4dc62652246f9e0dea264faca87820c8dfec567
Cristian Leos <cristianleos2022@gmail.com>
2023-06-07 17:37:13 -0600

    test23

--- es/template/footer.php
+++ es/template/footer.php
@@ -103,11 +103,12 @@ políticas de cambios, devolucuón y reembolsos</a>
                         <div class="large-12 small-11 grid-x mx-auto">
                             <div class="large-6 small-12 fs-6 text-1 text-center">
                                 <span class="fw-bold">CERTIFICADO DE CALIDAD</span>
+                                <br>
                             </div>
                             <div class="large-6 small-12 fs-6 text-2 text-center">ISO 9001:2015 NMX-CC-9001-IMNC-2015</div>
                             <div class="large-6 small-12 fs-6 text-2 text-center">15-08-2022/JM-43/14-08-2025</div>
                             <br>
-                            <div class="large-6 small-12 fs-9 text-2 text-center">El precio de nuestros productos puede cambiar sin previo aviso. <br>
+                            <div class="large-6 small-12 fs-9 text-2 text-center mt-3">El precio de nuestros productos puede cambiar sin previo aviso. <br>
                             El empaque del producto puede cambiar sin previo aviso.</div>
                             <img class="large-6 small-12 fs-6 text-2 mx-auto d-block" style="width: 200px;" src="../img/full.png" alt="">
                         </div>
