b1efeda8444ea20cb0cfc87ecfdb700267ab6809
Cruz Bonilla Alejandro <alex.139139.cb@gmail.com>
2023-07-13 11:28:42 -0600

    testing

--- es/blog.php
+++ es/blog.php
@@ -4,58 +4,58 @@
 
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Prueba discreta a domicilio para detección de infecciones de transmisión sexual. Con certificado ISO 9000 y COFEPRIS, médicos altamente capacitados con tecnología de punta.">
-    <meta http-equiv="cache-control" content="no-cache" />
-    <title>ChektAhora</title>
-    <?php include 'template/favicon.php'; ?>
-    <link rel="stylesheet" href="../css/app.css?update=<?php echo constant('UPDATE'); ?>">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap" rel="stylesheet">
-    <script src="https://www.google.com/recaptcha/api.js?render=6LeRAI8eAAAAAGYdXRWWmQ3V6XqzSjeZ3LCLXJwh"></script> 
-    <link rel="preconnect" href="https://www.google.com">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="preconnect" href="https://www.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="../css/blog.css">
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Prueba discreta a domicilio para detección de infecciones de transmisión sexual. Con certificado ISO 9000 y COFEPRIS, médicos altamente capacitados con tecnología de punta.">
+  <meta http-equiv="cache-control" content="no-cache" />
+  <title>ChektAhora</title>
+  <?php include 'template/favicon.php'; ?>
+  <link rel="stylesheet" href="../css/app.css?update=<?php echo constant('UPDATE'); ?>">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap" rel="stylesheet">
+  <script src="https://www.google.com/recaptcha/api.js?render=6LeRAI8eAAAAAGYdXRWWmQ3V6XqzSjeZ3LCLXJwh"></script>
+  <link rel="preconnect" href="https://www.google.com">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="preconnect" href="https://www.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="../css/blog.css">
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script>
     $(document).ready(function() {
       $("#myModal").modal();
     });
   </script>
   <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NNR8MBT');</script>
-<!-- End Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-NNR8MBT');
+  </script>
+  <!-- End Google Tag Manager -->
 </head>
 
 <body>
 
-<!--
-<div class="popup">
-  <div class="popup-content">
-    <span class="close">&times;</span>
-    <p>Les Informamos que el próximo <b>20 de Marzo no tendremos servicio</b> para toma de muestras a domicilio ni en el laboratorio, volviendo a nuestros <b>Horarios habituales el martes 21 de marzo del 2023.</b> </p>
-    <p>Lamentamos los inconvenientes y agradecemos su compresión.</p>
-    <p>Atentamente <br> ChektAhora</p>
-  </div>
-</div>
--->
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NNR8MBT"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NNR8MBT" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
@@ -71,110 +71,100 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     </div>
   </div>
 
-    <div class="fluid" id="home">
+  <div class="fluid" id="home">
     <?php include 'template/header.php'; ?>
 
-        <!--Banner Principal de Blog-->
+    <!--Banner Principal de Blog-->
+    <div class="banner">
+      <img src="../img/woaoooo.jpg" alt="Descripción de la imagen">
+      <div class="texto-imagen">
+        <h1>Nuestro BLOG</h1>
+      </div>
+    </div>
+    <!--Final de Banner Principal de Blog-->
 
-        
-        <div class="banner">
-           <img src="../img/woaoooo.jpg" alt="Descripción de la imagen">
-            <div class="texto-imagen">
-             <h1>Nuestro BLOG</h1>
-             </div>
+    <!--Inicio de 2da Sección-->
+    <div class="seccion">
+      <div class="imagen">
+        <img src="../img/cristiano.jpg" alt="Imagen de ejemplo">
+      </div>
+      <div class="contenido">
+        <h1>Preguntas frecuentes sobre Infecciones de Transmisión Sexual (ITS)</h1>
+        <p class="fecha">Fecha de publicación: 15 mayo 2023</p>
+        <p class="descripcion">Las Infecciones de Transmisión Sexual (ITS) <br> son un tema que muchas personas evitan discutir debido <br> al estigma social sobre el diagnóstico y la falta de información precisa.
+          .</p>
+        <a href="#" class="leer-mas">Leer más</a>
+      </div>
+    </div>
+    <!--Fianl de 2da sección-->
+
+    <!--Inicio de 3ra sección-->
+    <div class="seccion-azul d-none">
+      <div class="titulo">
+        <h2>Noticias <br> por categoría</h2>
+        <div class="botones">
+          <a href="#" class="boton">ITS</a>
+          <a href="#" class="boton">ITS</a>
+          <a href="#" class="boton">ITS</a>
+          <a href="#" class="boton">ITS</a>
+          <a href="#" class="boton">ITS</a>
+          <a href="#" class="boton">ITS</a>
         </div>
-
-        <!--Final de Banner Principal de Blog-->
-
-        <!--Inicio de 2da Sección-->
-
-        <div class="seccion">
-    <div class="imagen">
-      <img src="../img/cristiano.jpg" alt="Imagen de ejemplo">
-    </div>
-    <div class="contenido">
-      <h2>ITS</h2>
-      <h1>Preguntas frecuentes sobre Infecciones de Transmisión Sexual (ITS)</h1>
-      <p class="fecha">Fecha de publicación: 15 mayo 2023</p>
-      <p class="descripcion">Las Infecciones de Transmisión Sexual (ITS) <br> son un tema que muchas personas evitan discutir debido <br> al estigma social sobre el diagnóstico y la falta de información precisa. 
-.</p>
-      <a href="#" class="leer-mas">Leer más</a>
-    </div>
-  </div>
-
-
-  <!--Fianl de 2da sección-->
-
-  <!--Inicio de 3ra sección-->
-
-  <div class="seccion-azul">
-    <div class="titulo">
-      <h2>Noticias <br> por categoría</h2>
-      <div class="botones">
-        <a href="#" class="boton">ITS</a>
-        <a href="#" class="boton">ITS</a>
-        <a href="#" class="boton">ITS</a>
-        <a href="#" class="boton">ITS</a>
-        <a href="#" class="boton">ITS</a>
-        <a href="#" class="boton">ITS</a>
       </div>
     </div>
-  </div>
+    <!--Fin de 3ra seccion-->
 
-  <!--Fin de 3ra seccion-->
+    <div class="large-12 medium-12 small-12 grid-padding-x text-center align-self-middle spaceSection my-4 d-none">
+      <h2 style="font-size: 35px;" class="morado">
+        <strong>Otros artículos de interés<strong>
+      </h2>
+    </div>
 
-  <!--Inicio de 4ta sección-->
-
-  <div class="seccion">
-    <div class="elemento">
-      <img src="../img/cristiannn.jpg" alt="Imagen 1">
-      <div class="contenido">
-        <h2>ITS</h2>
-        <h3>Preguntas frecuentes sobre Infecciones de Transmisión Sexual (ITS)</h3>
-        <a href="#" class="boton">Leer</a>
+    <!--Inicio de 4ta sección-->
+    <div class="seccion d-none">
+      <div class="elemento">
+        <img src="../img/cristiannn.jpg" alt="Imagen 1">
+        <div class="contenido">
+          <h3>Preguntas frecuentes sobre Infecciones de Transmisión Sexual (ITS)</h3>
+          <a href="#" class="boton">Leer</a>
+        </div>
+      </div>
+      <div class="elemento">
+        <img src="../img/cristiannn.jpg" alt="Imagen 2">
+        <div class="contenido">
+          <h3>Preguntas frecuentes sobre Infecciones de Transmisión Sexual (ITS)</h3>
+          <a href="#" class="boton">Leer</a>
+        </div>
+      </div>
+      <div class="elemento">
+        <img src="../img/cristiannn.jpg" alt="Imagen 3">
+        <div class="contenido">
+          <h3>Preguntas frecuentes sobre Infecciones de Transmisión Sexual (ITS)</h3>
+          <a href="#" class="boton">Leer</a>
+        </div>
       </div>
     </div>
-    <div class="elemento">
-      <img src="../img/cristiannn.jpg" alt="Imagen 2">
-      <div class="contenido">
-        <h2>ITS</h2>
-        <h3>Preguntas frecuentes sobre Infecciones de Transmisión Sexual (ITS)</h3>
-        <a href="#" class="boton">Leer</a>
+    <!--Fin de 4ta de sección-->
+
+    <!--Inicio de 5ta seccion-->
+    <section class="image-section">
+      <div class="image-container">
+        <div class="text">
+          <h2>En ChektAhora Nos preocupamos por tu salud</h2>
+        </div>
       </div>
-    </div>
-    <div class="elemento">
-      <img src="../img/cristiannn.jpg" alt="Imagen 3">
-      <div class="contenido">
-        <h2>ITS</h2>
-        <h3>Preguntas frecuentes sobre Infecciones de Transmisión Sexual (ITS)</h3>
-        <a href="#" class="boton">Leer</a>
-      </div>
-    </div>
+    </section>
+    <!--Fin de la 5ta seccion-->
+
+    <?php include 'template/footer.php'; ?>
   </div>
+  <script src="../js/main.min.js?update=<?php echo constant('UPDATE'); ?>"></script>
+  <script src="../js/main/owl.carousel.min.js"></script>
+  <script src="../js/pop-up.js?update=<?php echo constant('UPDATE'); ?>"></script>
 
-  <!--Fin de 4ta de sección-->
-
-
-  <!--Inicio de 5ta seccion-->
-
-  <section class="image-section">
-  <div class="image-container">
-    <img src="../img/woaoooo.jpg" alt="Descripción de la imagen">
-    <h2>En ChektAhora <br> Nos preocupamos por tu  salud</h2>
-  </div>
-</section>
-
-<!--Fin de la 5ta seccion-->
-
-        <?php include 'template/footer.php'; ?>
-    </div>
-    <script src="../js/main.min.js?update=<?php echo constant('UPDATE'); ?>"></script>
-    <script src="../js/main/owl.carousel.min.js"></script>
-    <script src="../js/pop-up.js?update=<?php echo constant('UPDATE'); ?>"></script>
-
-    <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
 </body>
 
-</html> 
+</html>
